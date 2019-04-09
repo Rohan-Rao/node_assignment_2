592d595530c6eadeedf12d46c8b02da213d65131
Rohan-Rao <46103200+Rohan-Rao@users.noreply.github.com>
2019-04-09 11:01:35 +0530

    Add files via upload

---
+++ assign2.js
@@ -0,0 +1,72 @@
+//Promise approach
+getCustomer(1).
+                then( (customer) => {
+                        console.log('Customer: ', customer);
+                        if (customer.isGold){
+                            getTopMovies().
+                                            then( (movies) => {
+                                                console.log('Top movies: ', movies);
+                                                sendEmail(customer.email, movies).
+                                                                                    then( () => {
+                                                                                        console.log('Email sent...')
+                                                                                    })
+                                            })
+                        }
+                })
+
+//Async / Awiat approach
+doAsyncOpr();
+async function doAsyncOpr(){
+   const custObj  = await getCustomer(1);
+   console.log('Customer: ', custObj);
+   if(custObj.isGold){
+        const movies   = await getTopMovies();
+        console.log('Top movies: ', movies);
+        await sendEmail(custObj.email, movies);
+        console.log('Email sent...')
+   }
+}
+
+//Callback approach
+/*
+getCustomer(1, (customer) => {
+    console.log('Customer: ', customer);
+    if (customer.isGold) {
+      getTopMovies((movies) => {
+        console.log('Top movies: ', movies);
+        sendEmail(customer.email, movies, () => {
+          console.log('Email sent...')
+        });
+      });
+    }
+  });
+  */
+function getCustomer(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ 
+              id: 1, 
+              name: 'Mosh Hamedani', 
+              isGold: true, 
+              email: 'email' 
+            });
+          }, 4000); 
+    });
+  }
+  
+  function getTopMovies() {
+      return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            resolve(['movie1', 'movie2']);
+          }, 4000);
+      });
+  }
+  
+  function sendEmail(email, movies) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+          }, 4000);
+    })
+  }
+  
